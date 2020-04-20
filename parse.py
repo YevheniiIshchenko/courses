e2b0ad3e1249ac38223beb248f589017d8ab9e09
YevheniiIshchenko <zhekaibubon@gmail.com>
2020-04-20 20:02:01 +0300

    parse.py is done

--- parse.py
+++ parse.py
@@ -1,2 +1,21 @@
-def parse(query: str) ->dict:
-  return {]
+def parse(query: str) -> dict:
+  res = {}
+  if '?' in query:
+    query = query.split("?")[1]
+  for i in query.split('&'):
+      if '=' in i:
+        p = i.split('=')
+        res[p[0]] = p[1]
+  return res
+
+if __name__ == '__main__':
+  assert parse('https://example.com/path/to/page?arg1=val1&arg2=val2') == {'arg1': 'val1', 'arg2': 'val2'}
+  assert parse('https://example.com/path/to/page? arg1 = val1 &arg2=val2&') == {' arg1 ': ' val1 ' , 'arg2': 'val2'}
+  assert parse('https://example.com/path/to/page?arg1=val1=val&arg2=val2') == {'arg1': 'val1', 'arg2': 'val2'}
+  assert parse('https://example.com/path/to/page?arg1=&arg2=val2') == {'arg1': '', 'arg2': 'val2'}
+  assert parse('http://example.com/?aaa=aaa') == {'aaa': 'aaa'}
+  assert parse('') == {}
+  assert parse('@@?aaa=aaa') == {'aaa':'aaa'}
+  assert parse('aaa=aaa') == {'aaa':'aaa'}
+  assert parse('=') == {'':''}
+  assert parse('http://example.com/?') == {}
